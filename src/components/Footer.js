40c312c48b8b9996a6356462811c4f9536aa2f2c
michyher <michelleherrera2000@yahoo.com>
2022-10-27 16:45:12 -0700

    made things pretty part one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
 
     return(
-        <div class="Footer">
+        <div className="footer">
             <p>CopyRight Lyssa & Michy</p>
         </div>
     )
